5d1e3fe58bacc6ef97c630afaa38648b1a3a40e1
Jan Reinhardt <Jan.Reinhardt@aitgmbh.de>
2019-06-24 22:40:52 +0200

    Created project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsolePong
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
